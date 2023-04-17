4a28679c6bbe00a051139152a124f5ae5078b1ad
Kamil Wojas <107795476+KamilWojas@users.noreply.github.com>
2023-04-17 21:02:10 +0200

    Add files via upload

---
+++ UTP3_WK_S23878/src/zad3/ProgLang.java
@@ -0,0 +1,109 @@
+package zad3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ProgLang {
+    private ArrayList<String> lines;
+    public ProgLang(String path) {
+        this.lines = new ArrayList<String>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                this.lines.add(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addOrCreate(LinkedHashMap<String, LinkedHashSet<String>> programmersMap, String key, String value) {
+        LinkedHashSet<String> element = programmersMap.computeIfAbsent(key, k -> new LinkedHashSet<String>());
+        element.add(value);
+    }
+
+    public LinkedHashMap<String, LinkedHashSet<String>> getLangsMap() {
+        LinkedHashMap<String, LinkedHashSet<String>> langsMap = new LinkedHashMap<String, LinkedHashSet<String>>();
+
+        for (String line : this.lines) {
+            String[] splittedLine = line.split("\t");
+            String lang = "";
+
+            for (int i = 0; i < splittedLine.length; i++) {
+                String element = splittedLine[i];
+
+                if (i == 0) {
+                    lang = element;
+                    continue;
+                }
+
+                this.addOrCreate(langsMap, lang, element);
+            }
+        }
+
+        return langsMap;
+    }
+
+    public LinkedHashMap<String, LinkedHashSet<String>> getProgsMap() {
+        LinkedHashMap<String, LinkedHashSet<String>> programmersMap = new LinkedHashMap<String, LinkedHashSet<String>>();
+
+        for (String line : this.lines) {
+            String[] splittedLine = line.split("\t");
+            String lang = "";
+
+            for (int i = 0; i < splittedLine.length; i++) {
+                String element = splittedLine[i];
+
+                if (i == 0) {
+                    lang = element;
+                    continue;
+                }
+
+                this.addOrCreate(programmersMap, element, lang);
+            }
+        }
+
+        return programmersMap;
+    }
+
+    public Map<String, LinkedHashSet<String>> getLangsMapSortedByNumOfProgs() {
+        return this.sorted(this.getLangsMap(),
+                Comparator.comparing((Map.Entry<String, LinkedHashSet<String>> entry) -> -entry.getValue().size())
+                        .thenComparing(Map.Entry::getKey, String::compareTo));
+    }
+
+    public Map<String, LinkedHashSet<String>> getProgsMapSortedByNumOfLangs() {
+        return this.sorted(this.getProgsMap(),
+                Comparator.comparing((Map.Entry<String, LinkedHashSet<String>> entry) -> -entry.getValue().size())
+                        .thenComparing(Map.Entry::getKey, String::compareTo));
+    }
+
+    public Map<String, LinkedHashSet<String>> getProgsMapForNumOfLangsGreaterThan(int i) {
+        return this.filtered(this.getProgsMap(), (value) -> value.getValue().size() > i);
+    }
+
+    public <T, U> Map<T, U> filtered(Map<T, U> map, Predicate<Map.Entry<T, U>> fn) {
+        return map.entrySet().stream().filter(fn).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+    public <T, U> Map<T, U> sorted(Map<T, U> map, Comparator<Map.Entry<T, U>> fn) {
+        return map.entrySet().stream()
+                .sorted(fn)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
+    }
+}
